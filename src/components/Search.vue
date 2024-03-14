d1ce3c20b67067ba31a7b537aeb6971599bef581
Jason Cameron <jasoncameron@Jasons-MacBook-Pro-2.local>
2024-03-14 10:41:13 -0400

    added local storage and search filtering

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -8,7 +8,7 @@ export default {
             filtered: false,
             search: '',
             results: '',
-            filteredResults: {places:[]},
+            filteredResults: { places: [] },
             postalCode: ''
         }
     },
@@ -17,6 +17,26 @@ export default {
     },
 
     methods: {
+
+        // This methods exports filteredResults to a csv file.
+        exportToCsv() {
+            const items = this.filteredResults.places;
+            const replacer = (key, value) => value === null ? '' : value;
+            const header = Object.keys(items[0]);
+            let csv = items.map(row => header.map(fieldName => JSON.stringify(row[fieldName], replacer)).join(','));
+            csv.unshift(header.join(','));
+            csv = csv.join('\r\n');
+            const blob = new Blob([csv], { type: 'text/csv' });
+            const url = window.URL.createObjectURL(blob);
+            const a = document.createElement('a');
+            a.setAttribute('hidden', '');
+            a.setAttribute('href', url);
+            a.setAttribute('download', 'places.csv');
+            document.body.appendChild(a);
+            a.click();
+            document.body.removeChild(a);
+        },
+
         /**
          * This method filters the places data based on the provided postal code.
          * 
@@ -25,15 +45,22 @@ export default {
             this.filtered = true;
             const filteredData = this.results.places.filter(obj => obj.formattedAddress.includes(this.postalCode));
             this.filteredResults.places = filteredData;
+            return filteredData;
         },
 
         searchPlaces() {
+            this.filtered = false;
             const apiKey = 'AIzaSyAEAPVrCILuoMxjcn0V2sVS_qVgJ6LFsDQ';
             const url = 'https://places.googleapis.com/v1/places:searchText';
             const requestBody = {
                 textQuery: this.search,
             };
 
+            // if the search results are already in local storage, use them instead of making a new request.
+            if (localStorage.getItem('searchResults') === this.results) {
+                this.results = JSON.parse(localStorage.getItem('searchResults'));
+                return;
+            }
             fetch(url, {
                 method: 'POST',
                 headers: {
@@ -52,17 +79,19 @@ export default {
                 .then(data => {
                     console.log('searching', data);
                     this.results = data;
+                    localStorage.setItem('searchResults', JSON.stringify(data)); // Adding data to local storage
+                    console.log(localStorage.getItem('searchResults'));
                 })
                 .catch(error => {
                     console.error('There was a problem with the fetch operation:', error);
                 });
-
+        },
+        mounted() {
+            // clear local storage on page load.
+            localStorage.clear();
         }
-    },
-    mounted() {
-        // this.searchPlaces();
     }
-};  
+    };  
 </script>
 
 <template>
@@ -78,18 +107,19 @@ export default {
     <section>
         <h2>Results</h2>
         <div v-if="filtered">
-            <PlacesCard v-for="place in filteredResults.places" :key="filteredResults.places.indexOf(place)" :displayName="place.displayName.text" :formattedAddress="place.formattedAddress" />
+            <PlacesCard v-for="place in filteredResults.places" :key="filteredResults.places.indexOf(place)"
+                :displayName="place.displayName.text" :formattedAddress="place.formattedAddress" />
         </div>
         <div v-else>
-            <PlacesCard v-for="place in results.places" :key="results.places.indexOf(place)" :displayName="place.displayName.text" :formattedAddress="place.formattedAddress" />
+            <PlacesCard v-for="place in results.places" :key="results.places.indexOf(place)"
+                :displayName="place.displayName.text" :formattedAddress="place.formattedAddress" />
         </div>
-        
+
     </section>
 </template>
 
 <style scoped>
 .pure-button-primary {
-   margin: 0 5px;
+    margin: 0 5px;
 }
 </style>
- 
